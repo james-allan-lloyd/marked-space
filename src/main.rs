c2b66b0fcde6069a8ae86d5c076cb924404d113c
James Lloyd <jlloyd@qxperts.io>
2023-11-25 08:43:39 +0100

    add reqwest blocking

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
-fn main() {
+use std::collections::HashMap;
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("Hello, world!");
+    let resp =
+        reqwest::blocking::get("https://httpbin.org/ip")?.json::<HashMap<String, String>>()?;
+    println!("{:#?}", resp);
+    Ok(())
 }
